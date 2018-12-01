456ebdecca6ec3f39199ddbf62739d63bb5738c6
Apipol Niyomsak <apipol.niyomsak@kingpower.com>
2018-12-01 13:42:29 +0700

    apply rate slowdown for /character route

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { INestApplication, INestExpressApplication } from '@nestjs/common'
 import { AppModule } from './app.module';
 import * as cors from 'cors'
+import * as slowdown from 'express-slow-down'
+
+bootstrap()
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cors())
-  await app.listen(process.env.PORT || 3000);
+  const app = await NestFactory.create(AppModule)
+  setup(app)
+  await app.listen(process.env.PORT || 3000)
 }
-bootstrap();
+
+function setup(app: INestApplication & INestExpressApplication) {
+  app.enable("trust proxy")
+  app.use(cors())
+  const speedLimiter = slowdown({
+    windowMs: 10 * 60 * 1000,
+    delayAfter: 180,
+    delayMs: 3 * 1000,
+  })
+  app.use('/character', speedLimiter)
+}
